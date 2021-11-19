d95eb7e8a6f96305e6103d5c3d1981804abea581
ppedroza404 <78894120+ppedroza404@users.noreply.github.com>
2021-11-18 22:28:47 -0600

    Update lista_doble_circular.cpp
    Update Lista Doble Circular

--- lista_doble_circular.cpp
+++ lista_doble_circular.cpp
@@ -32,27 +32,29 @@ NodoD* lista_doble_circular::dirDato(int dato)
 	NodoD* dir = NULL;
 	NodoD* aux = getCab();
 
-	while (aux != NULL && dir == NULL) {
+	// Recorre la lista desde la cabeza hasta el ultimo Nodo
+
+	for (int i = 1; i <= getLargo()-1; i++) {
+		// Si el valor del aux es igual al dato, iguala dir a dato
+		// en caso de que exista mas de un nodo con el mismo dato regresara la direccion del ultimo
 		if (aux->getDato() == dato) {
 			dir = aux;
 		}
-		else {
-			aux = aux->getSgte();
-		}
+		aux = aux->getSgte();
 	}
 	return dir;
 }
 
 NodoD* lista_doble_circular::dirUltimo()
-{// necesita cambios
-	NodoD* aux = getCab();
-	NodoD* ant = aux->getAnterior();
+{
+	NodoD* aux = NULL;
+	// Si la lista es vacia regresa null si no regresa en anterior a la cabeza 
 
-	while (aux != ant) {
-		aux = aux->getSgte();
+	if (!esVacia()) {
+		aux = getCab()->getAnterior();
 	}
-	ant = aux;
-	return ant;
+
+	return aux;
 }
 
 NodoD* lista_doble_circular::dirAnteriorDato(int dato)
@@ -60,13 +62,16 @@ NodoD* lista_doble_circular::dirAnteriorDato(int dato)
 	NodoD* dir = NULL;
 	NodoD* aux = getCab();
 
-	while (aux != NULL && dir == NULL) {
+	// Recorre la lista desde la cabeza hasta el ultimo Nodo
+
+	for (int i = 1; i <= getLargo() - 1; i++) {
+		// Si el valor del aux es igual al dato, iguala dir a dato
+		// en caso de que exista mas de un nodo con el mismo dato regresara la direccion del dato anterio 
+		// en el ultimo nodo
 		if (aux->getDato() == dato) {
 			dir = aux->getAnterior();
 		}
-		else {
-			aux = aux->getSgte();
-		}
+		aux = aux->getSgte();
 	}
 	return dir;
 }
@@ -76,13 +81,16 @@ NodoD* lista_doble_circular::dirAnteriorDato(NodoD* nodo)
 	NodoD* dir = NULL;
 	NodoD* aux = getCab();
 
-	while (aux != NULL && dir == NULL) {
+	// Recorre la lista desde la cabeza hasta el ultimo Nodo
+
+	for (int i = 1; i <= getLargo() - 1; i++) {
+		// Si el valor del aux es igual al dato, iguala dir a dato
+		// en caso de que exista mas de un nodo con el mismo dato regresara la direccion del ultimo 
+
 		if (aux->getDato() == nodo->getDato()) {
 			dir = aux->getAnterior();
 		}
-		else {
-			aux = aux->getSgte();
-		}
+		aux = aux->getSgte();
 	}
 	return dir;
 }
@@ -100,12 +108,7 @@ int lista_doble_circular::ultimo()
 { 
 	int dato = 0;
 	if (!esVacia()) {
-		NodoD* aux = getCab();
-		NodoD* ant = aux->getAnterior();
-		while (aux != ant) {
-			aux = aux->getSgte();
-		}
-		dato = aux->getDato();
+		dato = getCab()->getAnterior()->getDato();
 	}
 	return dato;
 }
@@ -120,13 +123,13 @@ bool lista_doble_circular::existe(int dato)
 	bool existe = false;
 	if (!esVacia()) {
 		NodoD* aux = getCab();
-		while (aux->getSgte() != NULL && !existe) {
+		for (int i = 1; i <= getLargo() - 1; i++) {
+			// Si el valor del aux es igual al dato, iguala dir a dato
+			// en caso de que exista mas de un nodo con el mismo dato regresara la direccion del ultimo 
 			if (aux->getDato() == dato) {
 				existe = true;
 			}
-			else {
-				aux = aux->getSgte();
-			}
+			aux = aux->getSgte();
 		}
 	}
 	return existe;
@@ -134,19 +137,16 @@ bool lista_doble_circular::existe(int dato)
 
 int lista_doble_circular::datoPos(int dato)
 {
-	NodoD* aux = getCab();
 	int pos = 0;
-	bool encontrado = false;
-	while (aux != NULL && encontrado != true) {
-		pos = pos + 1;
-		if (aux->getDato() == dato) {
-			encontrado = true;
-		}
-		else {
-			aux = aux->getSgte();
-			if (aux == NULL) {
-				pos = 0;
+	if (!esVacia()) {
+		NodoD* aux = getCab();
+		for (int i = 1; i <= getLargo() - 1; i++) {
+			// Si el valor del aux es igual al dato, iguala dir a dato
+			// en caso de que exista mas de un nodo con el mismo dato regresara la direccion del ultimo 
+			if (aux->getDato() == dato) {
+				pos = i;
 			}
+			aux = aux->getSgte();
 		}
 	}
 	return pos;
@@ -155,91 +155,67 @@ int lista_doble_circular::datoPos(int dato)
 void lista_doble_circular::desplegar()
 {
 	NodoD* aux = getCab();
-	if (aux != NULL) {
-		NodoD* sgte = aux->getAnterior();
 		std::cout << "ELEMENTOS EN LA LISTA:" << std::endl;
-		while (aux != sgte) {
-			cout << aux->getDato() << "->";
-			aux = aux->getSgte();
-		}
-		cout << aux->getDato() << "->";
-		cout << "Final \n\n\n";
+	for (int i = 1; i <= getLargo(); i++) {
+		cout << "(" << aux->getDato() <<")" << "->";
+		aux = aux->getSgte();
 	}
-	else {
-		std::cout << "NO HAY ELEMENTOS EN LA LISTA" << std::endl;
-	}
+	cout << "Final \n\n\n";
 }
 
 void lista_doble_circular::ordenarAscendente()
 {
+	// No Implementada
 }
 
 void lista_doble_circular::agregarInicio(int dato)
 {
 	NodoD* nuevo = new NodoD(dato); //Paso 1. Crear el nodo
-	nuevo->setSgte(getCab()); //Paso 2. Enlazar el nuevo nodo a la lista
-	//Paso 3 Reacomodar la lista con el nuevo nodo
+
+
 	if (!esVacia()) {
-		NodoD* sgte = nuevo->getSgte();
-		if (sgte->getSgte() != NULL) {
-			nuevo->setAnterior(sgte->getSgte());
-			sgte->setAnterior(nuevo);
-			sgte->getSgte()->setSgte(nuevo);
-			sgte->getSgte()->setAnterior(sgte);
-		}
+		nuevo->setSgte(getCab()); //Paso 2. Enlazar el nuevo nodo a la lista
+		nuevo->setAnterior(getCab()->getAnterior());
+		//Paso 3 Reacomodar la lista con el nuevo nodo
+
+		getCab()->getAnterior()->setSgte(nuevo);
+
+		getCab()->setAnterior(nuevo);
 	}
+	else {
+		nuevo->setSgte(nuevo);
+		nuevo->setAnterior(nuevo);
+	}
+	
 	setCab(nuevo);
 	setLargo(getLargo() + 1);
+	cout << getLargo();
 }
 
 void lista_doble_circular::agregarFinal(int dato)
 {
-	NodoD* nuevo = new NodoD(dato);
-	NodoD* aux = getCab();
+
+	NodoD* nuevo = new NodoD(dato); //Paso 1. Crear el nodo
 
 	if (!esVacia()) {
-		if (aux->getAnterior() == NULL) {
-			aux->setAnterior(nuevo);
-			aux->setSgte(nuevo);
-			nuevo->setAnterior(aux);
-			nuevo->setSgte(aux);
-		}
-		else {
-			aux->getAnterior()->setSgte(nuevo);
-			aux->setAnterior(nuevo);
-			nuevo->setAnterior(aux->getSgte());
-			nuevo->setSgte(aux);
-		}
-		setLargo(getLargo() + 1);
+		nuevo->setSgte(getCab()); //Paso 2. Enlazar el nuevo nodo a la lista
+		nuevo->setAnterior(getCab()->getAnterior());
+		//Paso 3 Reacomodar la lista con el nuevo nodo
+		getCab()->getAnterior()->setSgte(nuevo);
+		getCab()->setAnterior(nuevo);
 	}
+	else {
+		nuevo->setSgte(nuevo);
+		nuevo->setAnterior(nuevo);
+	}
+
+	setLargo(getLargo() + 1);
+	cout << getLargo();
 }
 
 bool lista_doble_circular::agregarAntesDe(int _datoRef, int _dato)
 {
-	bool agregado = false;
-
-	if (getCab()->getDato() == _dato) {
-		agregarInicio(_dato);
-		agregado = true;
-	}
-	else {
-		NodoD* aux = dirDato(_datoRef);
-		if (aux != NULL) {
-			NodoD* nuevo = new NodoD(_dato);//Paso 1. Crear el nodo
-
-			//Paso 2. Enlazar el nuevo nodo a la lista O sea guindar el nodo....
-			nuevo->setSgte(aux);
-			nuevo->setAnterior(aux->getAnterior());
-
-			//Paso 3. Reacomodar la lista con el nuevo nodo. Recuede el orden....
-			aux->getAnterior()->setSgte(nuevo);
-			aux->setAnterior(nuevo);
-			setLargo(getLargo() + 1);
-
-			agregado = true;
-		}
-	}
-	return agregado;
+	// No Implementada
 }
 
 bool lista_doble_circular::agregarDespuesDe(int _datoRef, int _dato)
@@ -284,7 +260,7 @@ bool lista_doble_circular::agregarDespuesDe(int _datoRef, int _dato)
 bool lista_doble_circular::borrar(int _dato)
 {
 	bool eliminado = false;
-	NodoD* aux = dirDato(_dato); //Paso 1. Señalo el nodo a eliminar
+	NodoD* aux = dirDato(_dato); //Paso 1. SeÃ±alo el nodo a eliminar
 	if (aux != NULL) {
 		//Paso 2. Reacomodar la lista sin el nodo
 		if (aux == getCab()) { // en caso de ser la cabeza la que se va a borrar
@@ -335,12 +311,12 @@ bool lista_doble_circular::borrarPos(int pos)
 	if (!esVacia()) {
 		while (aux == NULL && !eliminado) {
 			if (contador + 1 == pos) {
-				aux = ant->getSgte(); //Señalar el nodo a eliminar
+				aux = ant->getSgte(); //SeÃ±alar el nodo a eliminar
 				ant->setSgte(aux->getSgte()); //Reacomodar la lista con el nuevo nodo
 				ant->getSgte()->setAnterior(ant);
 			}
 			else if (contador == 1 && contador == pos) {
-				aux = getCab();  //señalar el nodo a eliminar
+				aux = getCab();  //seÃ±alar el nodo a eliminar
 				setCab(aux->getSgte()); //Reacomodar la lista con el nuevo nodo
 			}
 			else {
@@ -416,4 +392,86 @@ bool lista_doble_circular::intercambiar()
 
 void lista_doble_circular::eliminarTodasLasApariciones(int _dato)
 {
+	// No Implementada
+}
+
+void lista_doble_circular::insetDiferencia()
+{
+	NodoD* aux = getCab()->getSgte();
+	NodoD* nuevoaux = NULL;
+	int tam = getLargo();
+
+	for (int i = 2; i <= tam; i++) {
+
+		if (i % 2 == 0){
+			// Paso 2 inset el nodo nuevo en la lista
+			NodoD* nuevoaux = new NodoD();
+			nuevoaux->setDato(aux->getDato() - aux->getAnterior()->getDato());
+			nuevoaux->setAnterior(aux->getAnterior());
+			nuevoaux->setSgte(aux);
+			// Paso 3  Recomodar la lista con el nodo nuevo	
+			// Primero el puntero del vecino ** siempre
+			aux->getAnterior()->setSgte(nuevoaux);
+			aux->setAnterior(nuevoaux);
+
+			setLargo(getLargo() + 1);
+		}
+		aux = aux->getSgte();
+	}
+}
+
+void lista_doble_circular::desplegarInversa() {
+	int tam = getLargo();
+
+	if (!esVacia()) {
+		NodoD* aux = getCab()->getAnterior();
+		for (int i = 1; i <= tam;i++) {
+			cout << "(" << aux->getDato() << ")" << "->";
+			aux = aux->getAnterior();
+		}
+		cout << "Final \n\n\n";
+	}
+}
+
+float lista_doble_circular::promedio()
+{
+	float promedio = 0;
+	if (!esVacia()) {
+		NodoD* aux = getCab();
+
+		for (int i = 1; i <= getLargo(); i++) {
+			promedio += aux->getDato();
+			aux = aux->getSgte();
+		}
+		aux = aux->getSgte();
+		promedio = promedio / getLargo();
+	}
+	
+	return promedio;
+}
+
+void lista_doble_circular::borrarPosicionPar()
+{
+	NodoD* aux = getCab();
+	
+	int tam = getLargo();
+
+	for (int i = 0; i <= tam-1; i++) {
+		if (i % 2 == 0) {
+			if (i == 0) {				
+				aux->getSgte()->setAnterior(aux->getAnterior());
+				aux->getAnterior()->setSgte(aux->getSgte());
+				setLargo(getLargo() - 1);
+				setCab(aux->getSgte());
+			}
+			else {
+				aux->getSgte()->setAnterior(aux->getAnterior());
+				aux->getAnterior()->setSgte(aux->getSgte());
+				setLargo(getLargo() - 1);
+			}
+		}	
+		aux = aux->getSgte();
+	}
+		
+
 }
